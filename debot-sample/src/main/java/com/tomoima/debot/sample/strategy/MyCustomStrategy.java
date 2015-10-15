127dce8ea86114ef4ab93a9ab6016e39f76ff075
Shimokawa Takahiro <takahiro.1828@gmail.com>
2015-10-16 00:43:07 +0900

    Change the sample to use custom menu.

---
+++ debot-sample/src/main/java/com/tomoima/debot/sample/strategy/MyCustomStrategy.java
@@ -0,0 +1,14 @@
+package com.tomoima.debot.sample.strategy;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+
+import com.tomoima.debot.strategy.DebotStrategy;
+
+public class MyCustomStrategy extends DebotStrategy {
+
+    @Override
+    public void startAction(@NonNull Activity activity) {
+        // something
+    }
+}
