e58b79ce8fdf54bfd1d2bf280c2bbf1bc3e07100
tomoima525 <tomoima525@gmail.com>
2018-03-31 23:11:11 -0700

    refine codebase

--- debot/src/main/java/com/tomoima/debot/strategy/ShowActivityInfoStrategy.kt
+++ debot/src/main/java/com/tomoima/debot/strategy/ShowActivityInfoStrategy.kt
@@ -14,22 +14,20 @@ class ShowActivityInfoStrategy : DebotStrategy() {
     }
 
 
-    fun getJSONObject(intent: Intent): JSONObject {
+    private fun getJSONObject(intent: Intent): JSONObject {
         val data = JSONObject()
         val bundle = intent.extras ?: return data
 
         for (key in bundle.keySet()) {
             val value = bundle.get(key)
             if (value is String) {
-                var jsonArray: JSONArray? = null
                 val str = value.toString()
                 try {
-                    jsonArray = JSONArray(str)
+                    val jsonArray = JSONArray(str)
                     put(data, key, jsonArray)
                 } catch (e1: JSONException) {
                     put(data, key, str)
                 }
-
             } else {
                 put(data, key, value)
             }
